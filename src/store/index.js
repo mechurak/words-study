03873b30173665701c120bbe637639a6b52f1c5d
sshim from series9 <mechurak@gmail.com>
2018-11-11 16:43:20 +0900

    Sign in
    Reference:
    - https://medium.com/@oleg.agapov/basic-single-page-application-using-vue-js-and-firebase-part-2-143a3084266f

--- src/store/index.js
+++ src/store/index.js
@@ -38,6 +38,20 @@ export const store = new Vuex.Store({
         commit('setError', error.message)
         commit('setLoading', false)
       })
+    },
+    userSignIn ({commit}, payload) {
+      commit('setLoading', true)
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+      .then(firebaseUser => {
+        commit('setUser', {email: firebaseUser.user.email})
+        commit('setLoading', false)
+        commit('setError', null)
+        router.push('/home')
+      })
+      .catch(error => {
+        commit('setError', error.message)
+        commit('setLoading', false)
+      })
     }
   },
   getters: {}
