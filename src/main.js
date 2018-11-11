58ec3953af52efdca1257285490faa132f4675e5
sshim from series9 <mechurak@gmail.com>
2018-11-11 17:26:53 +0900

    Protected routes
    Referecne:
    - https://medium.com/@oleg.agapov/basic-single-page-application-using-vue-js-and-firebase-part-2-143a3084266f

--- src/main.js
+++ src/main.js
@@ -24,16 +24,18 @@ firebase.initializeApp(firebaseConfig)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  render: h => h(App),
-  created () {
-    firebase.auth().onAuthStateChanged((firebaseUser) => {
+const unsubscribe = firebase.auth()
+.onAuthStateChanged((firebaseUser) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
       if (firebaseUser) {
         store.dispatch('autoSignIn', firebaseUser)
       }
-    })
-  }
+    }
+  })
+  unsubscribe()
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import firebase from 'firebase'
 
 const routerOptions = [
   { path: '/', component: 'Landing' },
   { path: '/signin', component: 'Signin' },
   { path: '/signup', component: 'Signup' },
-  { path: '/home', component: 'Home' }
+  { path: '/home', component: 'Home', meta: { requiresAuth: true } }
 ]
 
 const routes = routerOptions.map(route => {
@@ -17,7 +18,19 @@ const routes = routerOptions.map(route => {
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = firebase.auth().currentUser
+  if (requiresAuth && !isAuthenticated) {
+    next('/signin')
+  } else {
+    next()
+  }
+})
+
+export default router
